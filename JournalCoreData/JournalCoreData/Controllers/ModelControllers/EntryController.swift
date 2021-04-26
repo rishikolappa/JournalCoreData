c62a23993c5d9b14f323c61ce8c3114b5da60356
rishikolappa <rishik@gmail.com>
2021-04-26 16:53:52 -0700

    Day 1 Journal

---
+++ JournalCoreData/JournalCoreData/Controllers/ModelControllers/EntryController.swift
@@ -0,0 +1,8 @@
+//
+//  EntryController.swift
+//  JournalCoreData
+//
+//  Created by Rishi Kolappa on 4/26/21.
+//
+
+import Foundation
---
+++ JournalCoreData/JournalCoreData/CoreData/Entry+Convenience.swift
@@ -0,0 +1,8 @@
+//
+//  Entry+Convenience.swift
+//  JournalCoreData
+//
+//  Created by Rishi Kolappa on 4/26/21.
+//
+
+import Foundation
